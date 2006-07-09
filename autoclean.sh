8282986b3bdf19a72d891e5a686f03369a9b85e3
wojtekka <wojtekka@63027e8e-9091-4708-a9e0-01b58f66871a>
2006-07-09 15:08:39 +0000

    Dokładnie porządki.

--- autoclean.sh
+++ autoclean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf config.* autom4te.cache Makefile.in Makefile INSTALL aclocal.m4 libtool ltmain.sh depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h src/.deps src/.libs src/*.o src/*.lo src/Makefile.in src/Makefile src/libydpdict.pc src/libydpdict.la pkgconfig/libydpdict.pc include/Makefile include/Makefile.in
+rm -rf config.* autom4te.cache Makefile.in Makefile INSTALL aclocal.m4 libtool ltmain.sh depcomp install-sh missing mkinstalldirs stamp* configure confdefs.h src/.deps src/.libs src/*.o src/*.lo src/Makefile.in src/Makefile src/libydpdict.pc src/libydpdict.la pkgconfig/libydpdict.pc include/Makefile include/Makefile.in compile
