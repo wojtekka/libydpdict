88eae9ea98cb03cb20d380acf5edcff897fd520c
wojtekka <wojtekka@63027e8e-9091-4708-a9e0-01b58f66871a>
2007-02-26 17:03:49 +0000

    Podbicie rocznika, ujednolicenie komentarzy.

--- src/ydpdict.c
+++ src/ydpdict.c
@@ -1,22 +1,22 @@
 /*
- * ydpdict support library
- * (C) Copyright 1998-2006 Wojtek Kaniewski <wojtekka@toxygen.net>
+ *  ydpdict support library
+ *  (C) Copyright 1998-2007 Wojtek Kaniewski <wojtekka@toxygen.net>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License Version
- * 2.1 as published by the Free Software Foundation.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License Version
+ *  2.1 as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
- * USA.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+ *  USA.
  *  
- * $Id$
+ *  $Id$
  */
 
 #include <sys/types.h>
