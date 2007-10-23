a83714f11f7fdbf02a07275eb03ef0bb84042259
wojtekka <wojtekka@63027e8e-9091-4708-a9e0-01b58f66871a>
2007-10-23 22:33:01 +0000

    Dodanie programiku do sprawdzania poprawności generowanego XML-a.

---
+++ misc/ydpxml.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ydpdict/ydpdict.h>
+
+#define DICT_PATH "/usr/local/share/ydpdict"
+
+int main(int argc, char **argv)
+{
+	ydpdict_t dict;
+	uint32_t i, j;
+
+	for (j = 0; j < 4; j++) {
+		int dicts[4] = { 100, 101, 200, 201 };
+		char dat[4096], idx[4096], prefix[128];
+
+		snprintf(dat, sizeof(dat), DICT_PATH "/dict%d.dat", dicts[j]);
+		snprintf(idx, sizeof(idx), DICT_PATH "/dict%d.idx", dicts[j]);
+
+		if (ydpdict_open(&dict, dat, idx, YDPDICT_ENCODING_UTF8) == -1) {
+			perror("ydpdict_open");
+			return 1;
+		}
+
+		snprintf(prefix, sizeof(prefix), "%s", (strrchr(dat, '/')) ? (strrchr(dat, '/') + 1) : dat);
+
+		for (i = 0; i < dict.word_count; i++) {
+			unsigned char *tmp;
+			FILE *f;
+	
+			printf("\r\033[K%s %d/%d %s", prefix, i, dict.word_count, dict.words[i]);
+			fflush(stdout);
+
+			tmp = ydpdict_read_xhtml(&dict, i);
+			
+			f = popen("xmllint - 1> /dev/null 2> /dev/null", "w");
+			fprintf(f, "%s", tmp);
+			if (pclose(f) != 0)
+				printf(" ERROR\n");
+			
+			free(tmp);
+		}
+
+		printf("\r[033K");
+		fflush(stdout);
+	
+		ydpdict_close(&dict);
+	}
+	
+	return 0;
+}
+
