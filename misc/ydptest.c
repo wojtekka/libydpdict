858d63260c45295cfb368bb3c112782310b5dc17
wojtekka <wojtekka@63027e8e-9091-4708-a9e0-01b58f66871a>
2007-10-29 21:37:45 +0000

    Możliwość pobrania słowa z dowolnego słownika.

--- misc/ydptest.c
+++ misc/ydptest.c
@@ -5,21 +5,37 @@
 
 int main(int argc, char **argv)
 {
+	char dat[128], idx[128];
 	ydpdict_t dict;
 	uint32_t i;
 	FILE *f;
+	int dictno;
+	const char *word;
 
-	if (ydpdict_open(&dict, DICT_PATH "/dict100.dat", DICT_PATH "/dict100.idx", YDPDICT_ENCODING_UTF8) == -1) {
+	if (argc == 1) {
+		dictno = 100;
+		word = "0";
+	} else if (argc == 2) {
+		dictno = atoi(argv[1]);
+		word = "0";
+	} else if (argc == 3) {
+		dictno = atoi(argv[1]);
+		word = argv[2];
+	} else {
+		fprintf(stderr, "usage: %s [dict] [word]\n", argv[0]);
+		return 1;
+	}
+	
+	snprintf(dat, sizeof(dat), "%s/dict%03d.dat", DICT_PATH, dictno);
+	snprintf(idx, sizeof(idx), "%s/dict%03d.idx", DICT_PATH, dictno);
+
+	if (ydpdict_open(&dict, dat, idx, YDPDICT_ENCODING_UTF8) == -1) {
 		perror("ydpdict_open");
 		return 1;
 	}
 
-	ydpdict_xhtml_set_title(&dict, "ydpdict");
-
-	if (argc < 2)
-		i = 0;
-	else if (!(i = atoi(argv[1])))
-		i = ydpdict_find(&dict, argv[1]);
+	if (!(i = atoi(word)))
+		i = ydpdict_find(&dict, word);
 
 	printf("ydpdict_find = %u\n", i);
 	
